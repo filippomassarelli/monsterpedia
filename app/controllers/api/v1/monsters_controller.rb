282026c5b12e7518b207dd169196f8c7408b4905
Filippo Massarelli <filippomassarelli@Filippos-MacBook-Pro-2.local>
2021-01-16 18:22:15 +0300

    CRUD functionalities added in controllers and tested with POSTMAN yey

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -1,10 +1,52 @@
 module Api 
     module V1 
         class MonstersController < ApplicationController
-            def Index
+
+            before_action :find_monster, only: [:show, :update, :destroy]
+
+            def index
                 monsters = Monster.order('created_at DESC');
                 render json: {status: 'SUCCESS', message:'Loaded monsters', data:monsters}, status: :ok 
             end
+
+            def show
+                render json: {status: 'SUCCESS', message:'Loaded monster', data:monster}, status: :ok 
+            end
+
+            def create
+                monster = Monster.new(monster_params)
+
+                if monster.save
+                    render json: {status: 'SUCCESS', message:'Saved monster', data:monster}, status: :ok 
+                else
+                    render json: {status: 'ERROR', message:'Monsters not saved', data:monster.errors}, status: :unprocessable_entity
+                end   
+            end
+
+            def update
+                if @monster.update(monster_params)
+                    render json: {status: 'SUCCESS', message:'Updated monster', data:@monster}, status: :ok 
+                else
+                    render json: {status: 'ERROR', message:'Monsters not updated', data:monster.errors}, status: :unprocessable_entity
+                end
+
+            end
+
+            def destroy
+                @monster.destroy
+                render json: {status: 'SUCCESS', message:'Deleted monster', data:@monster}, status: :ok 
+            end
+
+            private 
+
+            def monster_params
+                params.permit(:name, :tribe, :quote)
+            end
+
+            def find_monster
+                @monster = Monster.find(params[:id])
+            end
+
         end
     end
 end
